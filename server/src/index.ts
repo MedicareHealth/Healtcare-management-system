434d02581bae325c60e606d1f62509e4550bea6b
Pranav Kumar <pranav.kumar@edufied.edu.vn>
2024-01-21 17:07:55 +0530

    initialized server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello typescript")
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import cookinParser from 'cookie-parser'
+import compression from 'compression'
+
+const app = express()
+
+app.use(cors({
+    Credential: true
+}))
+
+app.use(compression())
+app.use(cookinParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+server.listen(8080, () => {
+    console.log("server running on http://localhost:8080")
+})
