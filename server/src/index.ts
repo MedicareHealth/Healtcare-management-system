8144c1d12ef0d233dcf2e5e95ab1d9197a67312a
Pranav Kumar <pranav.kumar@edufied.edu.vn>
2024-01-21 17:10:33 +0530

    initalized server

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,5 +17,5 @@ app.use(bodyParser.json())
 
 const server = http.createServer(app)
 server.listen(8080, () => {
-    console.log("server running on http://localhost:8080")
+    console.log("-----------server running on http://localhost:8080-----------")
 })
