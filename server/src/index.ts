b593e30080720fdaf7c65bb24971d50dbf452472
Pranav Kumar <pranav.kumar@edufied.edu.vn>
2024-01-22 16:09:38 +0530

    configured environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,8 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
 import http from 'http'
-
+import dotenv from 'dotenv'
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 const app = express()
 
 app.use(cors({
@@ -19,3 +20,6 @@ const server = http.createServer(app)
 server.listen(8080, () => {
     console.log("-----------server running on http://localhost:8080-----------")
 })
+
+const db_url = process.env.MONGO_URL
+console.log(db_url)
